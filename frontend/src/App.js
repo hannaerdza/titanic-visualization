70a50f428ff9b041849005acc51db12ca439d991
hannaerdza <146067154+hannaerdza@users.noreply.github.com>
2025-04-11 18:27:32 +0800

    Update App.js with latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import { Box, CssBaseline, AppBar, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Titanic Data Visualization</Typography>
+          </Toolbar>
+        </AppBar>
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
   );
 }
 
